af06142356acf4864f7512c20440d8e75d15e38e
Денис <Denis8.9@bk.ru>
2019-02-07 21:17:56 +0600

    module3-task1

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+    if (!is_readable($name)) {
+        return $result;
+    }
+    ob_start();
+    extract($data);
+    require $name;
+    $result = ob_get_clean();
+    return $result;
+};
+// показывать или нет выполненные задачи
+$show_complete_tasks = rand(0, 1);
+//функция подсчета задач
+function count_item($task_list, $project) {
+    $i = 0;
+    foreach ($task_list as $key => $types) {
+        if ($types['category_task'] == $project && !$types['complete_task']) {
+            $i++;
+        }
+    }
+    return $i;
+}
+//функция фильтрации задач
+function esc($str) {
+    $text = strip_tags($str);
+    return $text;
+}
